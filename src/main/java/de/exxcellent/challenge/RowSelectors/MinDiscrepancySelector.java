ce197d3877adffcd928755a862776e0408783d88
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-20 17:00:28 +0200

    Setup MinDiscrepancySelector class

---
+++ src/main/java/de/exxcellent/challenge/RowSelectors/MinDiscrepancySelector.java
@@ -0,0 +1,19 @@
+package de.exxcellent.challenge.RowSelectors;
+
+import java.util.ArrayList;
+
+public class MinDiscrepancySelector implements RowSelector{
+    private String firstColumn;
+    private String secondColumn;
+
+    public MinDiscrepancySelector(String _firstColumn, String _secondColumn){
+        this.firstColumn = _firstColumn;
+        this.secondColumn = _secondColumn;
+    }
+
+    @Override
+    public String[] selectRow(ArrayList<String[]> table) {
+        //TODO
+        return new String[0];
+    }
+}
