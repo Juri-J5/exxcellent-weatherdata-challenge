6991308e79af3a05484f7d801151b9841cc29de3
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-20 19:34:32 +0200

    write constructor test for CSVInputReader

--- src/test/java/de/exxcellent/challenge/inputReaders/CSVInputReaderTest.java
+++ src/test/java/de/exxcellent/challenge/inputReaders/CSVInputReaderTest.java
@@ -1,5 +1,10 @@
 package de.exxcellent.challenge.inputReaders;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 /**
  * Tests the {@link de.exxcellent.challenge.inputReaders.CSVInputReader} class.
  *
@@ -7,6 +12,32 @@ package de.exxcellent.challenge.inputReaders;
  */
 public class CSVInputReaderTest {
 
+    @Test
+    void testReaderCreation(){
+        try {                   //The filename does not exist and therefore the appropriate exception needs to be thrown
+            new CSVInputReader("not a file path or name");
+            Assertions.fail();
+        }catch (FileNotFoundException e){
+            //The file not being found and the appropriate error being thrown is correct
+        }catch (WrongFileTypeException e){
+            Assertions.fail();
+        }
+        try{                    //The fil is of a wrong type and therefore the appropriate exception needs to be thrown
+            new CSVInputReader("src/main/resources/de/exxcellent/challenge/test.txt");
+            Assertions.fail();
+        }catch(FileNotFoundException e){
+            Assertions.fail();
+        }catch (WrongFileTypeException e){
+            //The type of the file .txt and not .csv so this is the expected behaviour
+        }
+        try{                    //the call is correct and therefore no Exception should be thrown
+            new CSVInputReader("src/main/resources/de/exxcellent/challenge/football.csv");
+        }catch(Exception e){
+            Assertions.fail();
+        }
+    }
+
+    @Test
     void testReadData(){
         
     }
