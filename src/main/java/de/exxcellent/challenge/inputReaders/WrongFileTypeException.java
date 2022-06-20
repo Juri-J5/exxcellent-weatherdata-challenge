d8586b5a6b9b0cd0fbfe1c44382620ba1f7596b0
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-20 20:00:51 +0200

    Add constructor with message

--- src/main/java/de/exxcellent/challenge/inputReaders/WrongFileTypeException.java
+++ src/main/java/de/exxcellent/challenge/inputReaders/WrongFileTypeException.java
@@ -1,4 +1,7 @@
 package de.exxcellent.challenge.inputReaders;
 
 public class WrongFileTypeException extends Exception{
+    public WrongFileTypeException(String errorMessage){
+        super(errorMessage);
+    }
 }
