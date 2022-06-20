e5db48de3f3f59ff2db99a29a0e5ce18453d21e1
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-20 16:54:19 +0200

    Setup CSVInputReader with empty methods

---
+++ src/main/java/de/exxcellent/challenge/InputReaders/CSVInputReader.java
@@ -0,0 +1,15 @@
+package de.exxcellent.challenge.InputReaders;
+
+import java.util.ArrayList;
+
+public class CSVInputReader implements InputReader{
+    public CSVInputReader(String fileName) {
+        //TODO
+    }
+
+    @Override
+    public ArrayList<String[]> readData() {
+        //TODO
+        return null;
+    }
+}
