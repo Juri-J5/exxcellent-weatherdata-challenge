d36090fdef07bc3d9bbda2e5ae7b0263aa2f303f
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-21 10:48:32 +0200

    Write test for ProgrammingChallengeController

--- src/test/java/de/exxcellent/challenge/controllers/ProgrammingChallengeControllerTest.java
+++ src/test/java/de/exxcellent/challenge/controllers/ProgrammingChallengeControllerTest.java
@@ -1,5 +1,9 @@
 package de.exxcellent.challenge.controllers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 /**
  * Tests the {@link de.exxcellent.challenge.controllers.ProgrammingChallengeController} class.
  *
@@ -7,7 +11,9 @@ package de.exxcellent.challenge.controllers;
  */
 public class ProgrammingChallengeControllerTest {
 
+    @Test
     void testSolveWeatherTask(){
-
+        ProgrammingChallengeController controller = new ProgrammingChallengeController();
+        Assertions.assertEquals("14", controller.solveWeatherTask());
     }
 }
