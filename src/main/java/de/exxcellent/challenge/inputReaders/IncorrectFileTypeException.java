380949942a1b40d2f515895ed542fcbffe19c8a1
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-21 12:42:08 +0200

    Add missing comment to IncorrectFileTypeException

--- src/main/java/de/exxcellent/challenge/inputReaders/IncorrectFileTypeException.java
+++ src/main/java/de/exxcellent/challenge/inputReaders/IncorrectFileTypeException.java
@@ -1,5 +1,9 @@
 package de.exxcellent.challenge.inputReaders;
 
+/**
+ * An Exception that can be thrown, when a {@link de.exxcellent.challenge.inputReaders.InputReader} for a specific fileType
+ * is called with a file of an incorrect type.
+ */
 public class IncorrectFileTypeException extends Exception{
     public IncorrectFileTypeException(String errorMessage){
         super(errorMessage);
