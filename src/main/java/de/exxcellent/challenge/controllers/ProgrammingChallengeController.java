a9cfa488d8ea9e23a3714dcbdc92718c8b610ad1
Juri-J5 <philipp.jurinka@gmail.com>
2022-06-21 12:25:27 +0200

    add javadoc to all previously split methods

--- src/main/java/de/exxcellent/challenge/controllers/ProgrammingChallengeController.java
+++ src/main/java/de/exxcellent/challenge/controllers/ProgrammingChallengeController.java
@@ -17,11 +17,11 @@ import java.util.Optional;
 public class ProgrammingChallengeController {
 
     /**
-     * Solves the weatherdata challenge when called.
+     * Solves the weather data challenge when called.
      * Internally uses a CSVInputReader to read the weather.csv file and
      * a MinDiscrepancySelector to choose the row with minimal difference between
      * MnT and MxT.
-     * @return The first entry of the row, with minimal distance between MnT and MxT
+     * @return The entry in the "Day" column of the row, that has the minimal distance between "MnT" and "MxT"
      */
     public String solveWeatherTask() {
         return applyMinDiscrepancySelectorOnCSV("src/main/resources/de/exxcellent/challenge/weather.csv",
@@ -30,7 +30,16 @@ public class ProgrammingChallengeController {
                 "Day");
     }
 
-    public String applyMinDiscrepancySelectorOnCSV(String filePath, String firstColumnName, String secondColumnName, String targetColumn){
+    /**
+     * Applies a MinDiscrepancySelector on a given .csv file using a CSVInputReader and selects the cell specified by targetCell
+     * Both external classes are created using the respective private methods of this class.
+     * @param filePath the path, that belongs to the target .csv file
+     * @param firstComparedColumnName the first of the two columns the MinDiscrepancySelector will compare
+     * @param secondComparedColumnName the second of the two columns the MinDiscrepancySelector will compare
+     * @param targetColumn the column specifying the cell to be received as a return value
+     * @return the cell specified by targetColumn from the row with minimal difference between the given columns or an empty String if it could not be determined.
+     */
+    public String applyMinDiscrepancySelectorOnCSV(String filePath, String firstComparedColumnName, String secondComparedColumnName, String targetColumn){
         CSVInputReader reader = this.createCSVInputReader(filePath);
         if(null == reader)
             return "";
@@ -42,7 +51,7 @@ public class ProgrammingChallengeController {
         }
 
         //Passing Max first, so that difference should be positive even without MAth.abs
-        String[] selectedRow = this.applyMinDiscrepancySelector(readTable, firstColumnName, secondColumnName);
+        String[] selectedRow = this.applyMinDiscrepancySelector(readTable, firstComparedColumnName, secondComparedColumnName);
         if(null == selectedRow)
             return "";
 
@@ -50,6 +59,13 @@ public class ProgrammingChallengeController {
         return selectedCell;
     }
 
+    /**
+     * Creates a CSVInputReader on the file specified by the given path.
+     * This method catches and logs all exceptions thrown by the reader.
+     *
+     * @param filePath the path, that the CSVInputReader receives
+     * @return the created reader or null if it could not be created
+     */
     private CSVInputReader createCSVInputReader(String filePath){
         try{
             return new CSVInputReader(filePath);
@@ -61,6 +77,15 @@ public class ProgrammingChallengeController {
         return null;
     }
 
+    /**
+     * Uses the MinDiscrepancySelector class to select a row from the given table.
+     * This method logs any erroneous results of the selector.
+     *
+     * @param table the table the MinDiscrepancySelector will be applied on
+     * @param firstColumnName the first of the two columns the MinDiscrepancySelector will compare
+     * @param secondColumnName the second of the two columns the MinDiscrepancySelector will compare
+     * @return the row selected by the MinDiscrepancySelector or null if an error occured
+     */
     private String[] applyMinDiscrepancySelector(ArrayList<String[]> table, String firstColumnName, String secondColumnName){
         MinDiscrepancySelector selector = new MinDiscrepancySelector(firstColumnName,secondColumnName);
         String[] selectedRow = selector.selectRow(table);
@@ -74,6 +99,15 @@ public class ProgrammingChallengeController {
             return selectedRow;
         }
     }
+
+    /**
+     * Selects the cell from values, which is in the same column as the given columnToSelect.
+     *
+     * @param columns the names of the columns of which the columnToSelect should be one
+     * @param values the values from which the targeted columnToSelect should be selected
+     * @param columnToSelect the name of the desired column
+     * @return the cell from values, which is on the same position as columnToSelect is on columns
+     */
     private String selectEntry(String[] columns, String[] values, String columnToSelect){
         if(columns.length != values.length) {
             System.out.println("Failed to perform the task, because the selected row does not match the number of columns in the table.");
